cd6e96e3d28acd954abd072c4ec6be4591857f7c
knm6777 <knm6777@gmail.com>
2019-12-07 05:15:30 +0900

    Fourth commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello, world from GIT!");
 		Ststem.out.println("Change after First commit!");
+        System.out.println("Changes after Pushing to remote!");
 	}
 }
